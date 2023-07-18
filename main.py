e2a8b27fdb9af9982938c736f87921f2a940a2a1
Jon <jonathancirillo@outlook.com>
2023-07-18 14:57:34 -0400

    working add to db method

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -28,8 +27,21 @@ def index():
 def get_drinks():
     return {"drinks":"drink data"}
 
+def add_to_db(new_name, new_desc):
+    existing_drink = Drink.query.filter_by(name=new_name).first()
+    if existing_drink is None:
+        drink = Drink(name= new_name, description=new_desc)
+        db.session.add(drink)
+        db.session.commit()
+        print(f'Added: {drink}')
+    else:
+        print(f'Drink with name {new_name} already exists. Skipping.')
+
 with app.app_context():
     db.create_all()
+    add_to_db("test name", "test description")
+    add_to_db("soda", "very sweet")
+    print(Drink.query.all())
 
 if __name__ == '__main__':
     app.run(debug=True)
